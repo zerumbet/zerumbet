cce371b550adf2a8f133afc36839660d7f1aeb3d
tongchia <tongchia@live.com>
2019-06-25 13:41:00 +0800

    |> / ->;

--- example/process/04.process.js
+++ example/process/04.process.js
@@ -4,9 +4,9 @@ version:      1.0.1
 
 op1  => createGuestAccount@^2.0.1                 # 创建游客账号
 op2  => removeGuestAccount                        # 删除游客账号
-sub1 => sendValidationCode ||> (sub1, verifyCode) # 通过手机注册
-sub2 => OAuthAccessToken |> getUserInfo           # 第三方社交网络注册
-sub3 => createAccount |> sendNotify               # 创建账号
+sub1 => sendValidationCode ->> (sub1, verifyCode) # 通过手机注册
+sub2 => OAuthAccessToken -> getUserInfo           # 第三方社交网络注册
+sub3 => createAccount -> sendNotify               # 创建账号
 
 start -> op1 ->> (op2, (sub1, sub2) -> sub3 ) -> end 
 start -[timeout:1d]-> op2 -> end
