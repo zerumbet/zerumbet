ad512074a8f9777365e6107a9740e26f34bd637d
tongchia <tongchia@live.com>
2019-06-25 13:40:16 +0800

    |> / ->;

--- example/process/04.process.js
+++ example/process/04.process.js
@@ -8,6 +8,6 @@ sub1 => sendValidationCode ||> (sub1, verifyCode) # 通过手机注册
 sub2 => OAuthAccessToken |> getUserInfo           # 第三方社交网络注册
 sub3 => createAccount |> sendNotify               # 创建账号
 
-start |> op1 ||> (op2, (sub1, sub2) |> sub3 ) |> end 
-start |timeout:1d|> op2 -> end
+start -> op1 ->> (op2, (sub1, sub2) -> sub3 ) -> end 
+start -[timeout:1d]-> op2 -> end
 `
